6740c99e2516f11bdfef541db51e91e33e44a95d
Kristof Gazso <kristofgazso@Kristofs-MBP.cust.communityfibre.co.uk>
2023-03-23 21:52:31 +0000

    updated to use env var

--- packages/bundler/src/GethTracer.ts
+++ packages/bundler/src/GethTracer.ts
@@ -53,7 +53,7 @@ function modifyTx(tx: TransactionRequest): TransactionRequestModified {
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export async function debug_traceCall (provider: JsonRpcProvider, tx: Deferrable<TransactionRequest>, options: TraceOptions): Promise<TraceResult | any> {
-  const anvilProvider = new JsonRpcProvider("http://arbitrum-anvil-development.up.railway.app")
+  const anvilProvider = new JsonRpcProvider(process.env.ANVIL_URL)
   const url = provider.connection.url;
   const blockNumber = await provider.getBlockNumber();
   await anvilProvider.send('anvil_reset', [{
